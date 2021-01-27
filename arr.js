354775a39075a454578e5985f270af58188d24f4
nkakalov <nkakalov>
2021-01-27 10:36:47 +0300

    save1

---
+++ arr.js
@@ -0,0 +1,8 @@
+let arr = ["cat","dog","parrot","hours"]
+
+console.log(arr)
+arr.forEach(console.log)
+//все элементы массива и их индекс 
+
+//найдем индекс нужного элемента 
+console.log('parrot:',arr.indexOf("parrot",0))
